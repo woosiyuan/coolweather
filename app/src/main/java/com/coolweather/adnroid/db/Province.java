539a3b1a85916cde4597f638d50c8144aa62dfbb
wusiyuan <573620778@qq.com>
2017-04-10 17:56:02 +0800

    加入创建数据库和标的各项配置。

---
+++ app/src/main/java/com/coolweather/adnroid/db/Province.java
@@ -0,0 +1,36 @@
+package com.coolweather.adnroid.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wu on 2017/4/10.
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
